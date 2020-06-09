18345f7c3c54090e836a7c07f2f34409dc2ada38
Alyson Matheus <alyson1907@gmail.com>
2020-06-08 22:40:15 -0300

    [WIP] +encoding OK! :tada: @3-kyu/RailFenceCipher

---
+++ 3-kyu/RailFenceCipher_EncodingAndDecoding.js
@@ -0,0 +1,45 @@
+const assert = require('assert')
+const encodeRailFenceCipher = (string, numberRails) => {
+  const setupRails = (letters, numberRails) => {
+    let sum = 1
+    let currNumber = 0
+    const rails = letters.map((char, i) => {
+      if ((currNumber === 0 && i !== 0) || currNumber === numberRails - 1) sum = (-1 * sum)
+      const res = currNumber + 1
+      currNumber += sum
+      return res
+    })
+    return rails
+  }
+
+  const catString = (letters, rails, numberRails) => {
+    let str = ''
+    // i: number in rail we are looking for
+    for (let i = 1; i <= numberRails; i++) {
+      letters.forEach((char, j) => {
+        // concatenating
+        if (rails[j] === i) {
+          str += letters[j]
+          console.log(str)
+        }
+      })
+    }
+    return str
+  }
+
+  if (!string) return ''
+  const letters = string.split('')
+  const rails = setupRails(letters, numberRails)
+  const finalStr = catString(letters, rails, numberRails)
+  return finalStr
+}
+
+const decodeRailFenceCipher = (string, numberRails) => {
+  if (!string) return ''
+
+}
+
+encodeRailFenceCipher("WEAREDISCOVEREDFLEEATONCE", 3)
+// assert.equal(encodeRailFenceCipher("WEAREDISCOVEREDFLEEATONCE", 3), "WECRLTEERDSOEEFEAOCAIVDEN")
+// assert.equal(decodeRailFenceCipher("WECRLTEERDSOEEFEAOCAIVDEN", 3), "WEAREDISCOVEREDFLEEATONCE")
+// assert.equal(encodeRailFenceCipher("Hello, World!", 3), "Hoo!el,Wrdl l")
