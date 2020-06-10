f28530d26dd68cf5f86d03b2b2b9bf6bccfc041a
Alyson Matheus <alyson1907@gmail.com>
2020-06-10 02:27:20 -0300

    ^decoding OK! ttada

--- 3-kyu/RailFenceCipher_EncodingAndDecoding.js
+++ 3-kyu/RailFenceCipher_EncodingAndDecoding.js
@@ -1,18 +1,20 @@
 const assert = require('assert')
 // https://www.codewars.com/kata/58c5577d61aefcf3ff000081/train/javascript
 // Cycle = (numberRails * 2) - 2
-const setupRails = (letters, numberRails) => {
-  const cycle = (numberRails * 2) - 2
-  let currNumber = 0
-  const rails = letters.map(char => {
-    if (currNumber === cycle) currNumber = 0
-    currNumber++
-    return currNumber
-  })
-  return rails
-}
+
 
 const encodeRailFenceCipher = (string, numberRails) => {
+  const setupRails = (letters, numberRails) => {
+    const cycle = (numberRails * 2) - 2
+    let currNumber = 0
+    const rails = letters.map(char => {
+      if (currNumber === cycle) currNumber = 0
+      currNumber++
+      return currNumber
+    })
+    return rails
+  }
+
   const catString = (letters, rails, numberRails) => {
     let str = ''
     // i: number in rail we are looking for
@@ -31,28 +33,37 @@ const encodeRailFenceCipher = (string, numberRails) => {
   const letters = string.split('')
   const rails = setupRails(letters, numberRails)
   const encoded = catString(letters, rails, numberRails)
-  console.log(JSON.stringify(rails))
   return encoded
 }
 
 const decodeRailFenceCipher = (string, numberRails) => {
+  const setupRails = (letters, numberRails) => {
+    let sum = 1
+    let currNumber = 0
+    const rails = letters.map((char, i) => {
+      if ((currNumber === 0 && i !== 0) || currNumber === numberRails - 1) sum = (-1 * sum)
+      const res = currNumber + 1
+      currNumber += sum
+      return res
+    })
+    return rails
+  }
+
   if (!string) return ''
   const letters = string.split('')
+  const cycle = (numberRails * 2) - 2
   const rails = setupRails(letters, numberRails)
-  const breakSize = parseInt(string.length / numberRails)
-  let currBreakIdx = 0
-  const strParts = []
-  let decoded = ''
-  while (currBreakIdx < string.length) {
-    strParts.push(string.slice(currBreakIdx, currBreakIdx + breakSize))
-    currBreakIdx += breakSize
+  let currCipherNumber = 1
+  let letterPos = 0
+  while (letterPos < letters.length) {
+    const i = rails.findIndex(el => el === currCipherNumber)
+    if (i !== -1) {
+      rails[i] = letters[letterPos]
+      letterPos++
+    } else currCipherNumber++
   }
-  console.log(strParts)
-
-
+  return rails.join('')
 }
-encodeRailFenceCipher("WEAREDISCOVEREDFLEEATONCE", 3)
-decodeRailFenceCipher("WECRLTEERDSOEEFEAOCAIVDEN", 3)
-// assert.equal(encodeRailFenceCipher("WEAREDISCOVEREDFLEEATONCE", 3), "WECRLTEERDSOEEFEAOCAIVDEN")
+assert.equal(encodeRailFenceCipher("WEAREDISCOVEREDFLEEATONCE", 3), "WECRLTEERDSOEEFEAOCAIVDEN")
 // assert.equal(decodeRailFenceCipher("WECRLTEERDSOEEFEAOCAIVDEN", 3), "WEAREDISCOVEREDFLEEATONCE")
-// assert.equal(encodeRailFenceCipher("Hello, World!", 3), "Hoo!el,Wrdl l")
+assert.equal(encodeRailFenceCipher("Hello, World!", 3), "Hoo!el,Wrdl l")
