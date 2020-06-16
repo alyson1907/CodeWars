12ebbf5eae15a2ceb82dc5474c7006370d755cb1
Alyson Matheus <alyson1907@gmail.com>
2020-06-16 20:56:48 -0300

    more tests @BinomialExpansion.js

--- 3-kyu/BinomialExpansion.js
+++ 3-kyu/BinomialExpansion.js
@@ -72,4 +72,11 @@ const expand = (expr) => {
   console.log(power, signalsInverted, expression);
 };
 
-console.log(expand('(-2k-3)^3'));
+console.log(expand("(x+1)^2"));      // returns "x^2+2x+1"
+console.log(expand("(p-1)^3"));      // returns "p^3-3p^2+3p-1"
+console.log(expand("(2f+4)^6"));     // returns "64f^6+768f^5+3840f^4+10240f^3+15360f^2+12288f+4096"
+console.log(expand("(-2a-4)^0"));    // returns "1"
+console.log(expand("(-12t+43)^2"));  // returns "144t^2-1032t+1849"
+console.log(expand("(r+0)^203"));    // returns "r^203"
+console.log(expand("(-x-1)^2"));     // returns "x^2+2x+1"
+
