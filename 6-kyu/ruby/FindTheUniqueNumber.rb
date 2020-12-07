4e9a42b1cd4e37cec45fe4713d1941d80c59491d
Alyson Maruyama <alyson1907@gmail.com>
2020-12-07 19:48:27 -0300

    +FindTheUniqueNumber.rb @6-kyu

---
+++ 6-kyu/ruby/FindTheUniqueNumber.rb
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/585d7d5adb20cf33cb000235/solutions/ruby/me/best_practice
+
+# There is an array with some numbers. All numbers are equal except for one. Try to find it!
+#   find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
+#   find_uniq([ 0, 0, 0.55, 0, 0 ]) == 0.55
+#   It’s guaranteed that array contains at least 3 numbers.
+
+# The tests contain some very huge arrays, so think about performance.
+
+def find_uniq(arr)
+  return arr.select{ |n| arr.one? { |c| c == n } }[0]
+end
